c0e56c0ab63c7f4d82ccb13e7f6fadae500f34e9
Lucas Caribé <lucasmoc47@gmail.com>
2021-08-19 16:18:53 -0300

    Adding online-store project

---
+++ online-store/precache-manifest.36e875334051191edef7e60b89171915.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92ccb1eae4b21479ef30b46e8c517aa",
+    "url": "/online-store/index.html"
+  },
+  {
+    "revision": "73e82d32d5fdb08cda4e",
+    "url": "/online-store/static/css/main.93521a6a.chunk.css"
+  },
+  {
+    "revision": "ea4f6135c2a6d89ec846",
+    "url": "/online-store/static/js/2.7ebb12a5.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/online-store/static/js/2.7ebb12a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e82d32d5fdb08cda4e",
+    "url": "/online-store/static/js/main.faffa81c.chunk.js"
+  },
+  {
+    "revision": "22496ea918f96246b60e",
+    "url": "/online-store/static/js/runtime-main.d46b2840.js"
+  }
+]);
