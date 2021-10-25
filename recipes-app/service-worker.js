9ee11c7d3386874c022099151506a63ba465317a
Lucas Caribé <lucasmoc47@gmail.com>
2021-10-25 02:24:40 -0300

    Recipes app

--- recipes-app/service-worker.js
+++ recipes-app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/online-store/precache-manifest.6eae5aadab7131f0ee885fe600c5af91.js"
+  "/recipes-app/precache-manifest.b4dacf50e536828dad41319a794a67ff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/online-store/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/recipes-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
